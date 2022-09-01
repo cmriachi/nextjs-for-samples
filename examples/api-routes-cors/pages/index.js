6a5cb4d62da061e65c3a2353bc94bb5356358362
cmriachi <104598291+cmriachi@users.noreply.github.com>
2022-09-01 15:08:18 -0700

    Update index.js

--- examples/api-routes-cors/pages/index.js
+++ examples/api-routes-cors/pages/index.js
@@ -3,8 +3,7 @@ export default function Index() {
     <p>
       To test the CORS route, open the console in a new tab on a different
       domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
-      a different method from those mentioned will be blocked by CORS
+      a different method from those mentioned will be blocked by CORS. Testing preview envs from PRs!
     </p>
-    <h>Testing preview envs from PRs!</h>
   )
 }
